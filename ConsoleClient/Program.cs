c74d34406d8a6257505977b47e89e910b848720f
Глеб Бутяев <iiaiias1992@gmail.com>
2022-09-13 11:32:43 +0300

    Добавил класс Message

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using MyMassenger1;
+
+internal class Program
 {
     private static void Main(string[] args)
+
     {
+        Message msg = new Message();
+
         Console.WriteLine("Начало проекта!");
+        Console.WriteLine(msg.ToString());
     }
 }
